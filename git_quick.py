820db3dbb8203cf2c8d2ce0aeb2122ceb6c695ae
QleanGG <guyguz1@gmail.com>
2023-12-14 19:53:05 +0200

    Auto commit

--- git_quick.py
+++ git_quick.py
@@ -11,7 +11,7 @@ try:
     commit_message = "Auto commit"
     repo.index.commit(commit_message)
 
-    repo.git.push('origin', 'master')
+    repo.git.push('master')
     print("It is done!")
 except Exception as e:
     print(f"An error occurred: {e}")
