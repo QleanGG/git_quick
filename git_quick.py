431652fcf60e3611974150d2898144783e22a3b9
QleanGG <guyguz1@gmail.com>
2023-12-15 00:13:05 +0200

    remove file before staging and add readme file

--- git_quick.py
+++ git_quick.py
@@ -1,17 +1,28 @@
+'''
+This is a git script to add all files, commit and push filed to your repository on github
+it takes the folder path from your os and enables to make the commits to your main branch.
+
+This will only work on main branch as it is a quick short script
+'''
+
 import os
 from git import Repo
+
 
 try:
     git_path = os.getcwd()
     repo = Repo(git_path)
     assert not repo.bare
 
-
+    # add all files but remove the script
     repo.index.add('*')
+    repo.index.remove('git_quick.py')
+    
+    # commit with default msg
     commit_message = "Auto commit"
     repo.index.commit(commit_message)
 
-    repo.git.push('master')
+    repo.git.push('origin', 'main')
     print("It is done!")
 except Exception as e:
     print(f"An error occurred: {e}")
---
+++ readme.md
@@ -0,0 +1,13 @@
+# git_quick
+
+This is a git script to add all files, commit and push filed to your repository on github
+it takes the folder path from your os and enables to make the commits to your main branch.
+
+**This will only work on main branch as it is a quick short script**
+
+## setup
+
+You will need to install the GitPython package with the command
+`pip install GitPython`
+
+## Happy quick git everybody!
